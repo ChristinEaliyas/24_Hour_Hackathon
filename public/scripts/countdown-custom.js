62a78fb8ba33d24c29edb39ed085f8d0a6e688d4
ishanchaskar <91770647+ishanchaskar@users.noreply.github.com>
2023-10-03 22:11:44 +0530

    Update countdown-custom.js

--- public/scripts/countdown-custom.js
+++ public/scripts/countdown-custom.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function() {
         $(function () {
-            $('#defaultCountdown').countdown({until: new Date(2023, 12, 20, 8)}); // year, month, date, hour
+            $('#defaultCountdown').countdown({until: new Date(2023, 09, 10, 8)}); // year, month, date, hour
         });
 });		
 
